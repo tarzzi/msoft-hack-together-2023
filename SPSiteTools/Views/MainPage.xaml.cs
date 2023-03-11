eaa37144b9d4c46751e4c42d8fcd68efe7672b22
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2023-03-11 15:36:25 +0200

    More functionality and responses

--- SPSiteTools/Views/MainPage.xaml.cs
+++ SPSiteTools/Views/MainPage.xaml.cs
@@ -3,8 +3,18 @@
 public partial class MainPage : ContentPage
 {
     public MainPage()
-	{
+	  {
 		InitializeComponent();
+
+    }
+
+  private void Button_Clicked(object sender, EventArgs e)
+  {
+    // find element with x:name = "entryID", change its text to "Hello World"
+    var entryID = this.FindByName<Entry>("entryID");
+    // get the x:name of the button that was clicked
+    var button = (Button)sender;
+    entryID.Text = button.Text;
 
     }
 }
